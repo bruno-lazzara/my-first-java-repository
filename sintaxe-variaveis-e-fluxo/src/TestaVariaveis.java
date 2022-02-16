13c46eb0b41dfffdf4f6c977e5bef19a929ecf39
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2022-02-16 20:51:20 -0300

    First commit from Eclipse

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,19 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+		
+		int idade;
+		idade = 27;
+		
+		System.out.println(idade);
+		
+		idade = 20 + 8;
+		
+		System.out.println("Minha idade é " + idade);
+		
+		idade = (5 * 5) + 4;
+		
+		System.out.print("Minha idade é " + idade + ", parabéns!");
+	}
+}
